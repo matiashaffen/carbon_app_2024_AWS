2dd22374719bd5a641e8c06985e81bc6d29b5143
matiashaffen <160602930+matiashaffen@users.noreply.github.com>
2024-04-14 15:28:56 +0200

    Update routes.py

--- capp/carbon_app/routes.py
+++ capp/carbon_app/routes.py
@@ -3,18 +3,19 @@ from capp.models import Transport
 from capp import db
 from datetime import timedelta, datetime
 from flask_login import login_required, current_user
-from capp.carbon_app.forms import BusForm, CarForm, PlaneForm, FerryForm, MotorbikeForm, BicycleForm, WalkForm
+from capp.carbon_app.forms import BusForm, CarForm, PlaneForm, FerryForm, MotorbikeForm, BicycleForm, WalkForm, TrainForm
 import json
 
 carbon_app=Blueprint('carbon_app',__name__)
 
 #Emissions factor per transport in kg per passemger km
-#Data from: http://efdb.apps.eea.europa.eu/?source=%7B%22query%22%3A%7B%22match_all%22%3A%7B%7D%7D%2C%22display_type%22%3A%22tabular%22%7D
-efco2={'Bus':{'Diesel':0.10231,'CNG':0.08,'Petrol':0.10231,'No Fossil Fuel':0},
+#Data from paper and: http://efdb.apps.eea.europa.eu/?source=%7B%22query%22%3A%7B%22match_all%22%3A%7B%7D%7D%2C%22display_type%22%3A%22tabular%22%7D
+efco2={'Bus':{'Diesel':0.025,'CNG':0.07,'Petrol':0.10231,'No Fossil Fuel':0},
     'Car':{'Petrol':0.18592,'Diesel':0.16453,'No Fossil Fuel':0},
-    'Plane':{'Petrol':0.24298},
-    'Ferry':{'Diesel':0.11131, 'CNG':0.1131, 'No Fossil Fuel':0},
-    'Motorbike':{'Petrol':0.09816,'No Fossil Fuel':0},
+    'Plane':{'Petrol':0.133},
+    'Ferry':{'Diesel':0.226, 'CNG':0.1131, 'No Fossil Fuel':0},
+    'Motorbike':{'Petrol':0.0852,'No Fossil Fuel':0},
+    'Train':{'No Fossil Fuel':0},
     'Scooter':{'No Fossil Fuel':0},
     'Bicycle':{'No Fossil Fuel':0},
     'Walk':{'No Fossil Fuel':0}}
@@ -23,6 +24,7 @@ efch4={'Bus':{'Diesel':2e-5,'CNG':2.5e-3,'Petrol':2e-5,'No Fossil Fuel':0},
     'Plane':{'Petrol':1.1e-4},
     'Ferry':{'Diesel':3e-5, 'CNG':3e-5,'No Fossil Fuel':0},
     'Motorbike':{'Petrol':2.1e-3,'No Fossil Fuel':0},
+    'Train':{'No Fossil Fuel':0},
     'Scooter':{'No Fossil Fuel':0},
     'Bicycle':{'No Fossil Fuel':0},
     'Walk':{'No Fossil Fuel':0}}
@@ -215,6 +217,32 @@ def new_entry_walk():
         return redirect(url_for('carbon_app.your_data'))
     return render_template('carbon_app/new_entry_walk.html', title='new entry walk', form=form)
 
+#New entry train
+@carbon_app.route('/carbon_app/new_entry_train', methods=['GET','POST'])
+@login_required
+def new_entry_train():
+    form = TrainForm()
+    if form.validate_on_submit():
+        kms = form.kms.data
+        fuel = form.fuel_type.data
+        transport = 'Train'
+        # kms = request.form['kms']
+        # fuel = request.form['fuel_type']
+
+        co2 = float(kms) * efco2[transport][fuel]
+        ch4 = float(kms) * efch4[transport][fuel]
+        total = co2+ch4
+
+        co2 = float("{:.2f}".format(co2))
+        ch4 = float("{:.2f}".format(ch4))
+        total = float("{:.2f}".format(total))
+
+        emissions = Transport(kms=kms, transport=transport, fuel=fuel, co2=co2, ch4=ch4, total=total, author=current_user)
+        db.session.add(emissions)
+        db.session.commit()
+        return redirect(url_for('carbon_app.your_data'))
+    return render_template('carbon_app/new_entry_train.html', title='new entry train', form=form)
+
 #Your data
 @carbon_app.route('/carbon_app/your_data')
 @login_required
@@ -228,7 +256,7 @@ def your_data():
     emissions_by_transport = db.session.query(db.func.sum(Transport.total), Transport.transport). \
         filter(Transport.date > (datetime.now() - timedelta(days=5))).filter_by(author=current_user). \
         group_by(Transport.transport).order_by(Transport.transport.asc()).all()
-    emission_transport = [0, 0, 0, 0, 0, 0, 0, 0]
+    emission_transport = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     first_tuple_elements = []
     second_tuple_elements = []
     for a_tuple in emissions_by_transport:
@@ -271,7 +299,7 @@ def your_data():
     kms_by_transport = db.session.query(db.func.sum(Transport.kms), Transport.transport). \
         filter(Transport.date > (datetime.now() - timedelta(days=5))).filter_by(author=current_user). \
         group_by(Transport.transport).order_by(Transport.transport.asc()).all()
-    kms_transport = [0, 0, 0, 0, 0, 0, 0, 0]
+    kms_transport = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     first_tuple_elements = []
     second_tuple_elements = []
     for a_tuple in kms_by_transport:
@@ -324,7 +352,13 @@ def your_data():
         index_walk = second_tuple_elements.index('Walk')
         kms_transport[7]=first_tuple_elements[index_walk]
     else:
-        kms_transport[7]   
+        kms_transport[7]
+
+    if 'Train' in second_tuple_elements:
+        index_train = second_tuple_elements.index('Train')
+        kms_transport[8]=first_tuple_elements[index_train]
+    else:
+        kms_transport[8]    
 
          #Emissions by date (individual)
     emissions_by_date = db.session.query(db.func.sum(Transport.total), Transport.date). \
